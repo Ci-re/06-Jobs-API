9a896ade89c3192a8a50c5c686eebeb8f8d4965a
Ci-re <cirecleen@gmail.com>
2022-04-30 20:28:53 +0100

    update

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,33 @@
-console.log('04 Store API')
+require('dotenv').config()
+// async errors
+require('express-async-errors')
+
+
+const express = require("express")
+const app = express();
+const connectDB = require("./db/connect")
+const notFoundMiddleWare = require("./middleware/not-found")
+const errorMiddleWare = require("./middleware/error-handler")
+const router = require("./routes/products")
+
+app.use(express.json())
+
+// routes
+
+app.get('/', (req, res) => {
+    res.send("Products Route")
+})
+
+app.use("/api/v1/products",router)
+app.use(notFoundMiddleWare);
+app.use(errorMiddleWare)
+
+
+
+const port = process.env.PORT || 5000
+
+const start = async () => {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`Server is listening on port ${port}`))
+}
+start();
---
+++ 04-store-api/starter/controllers/products.js
@@ -0,0 +1,41 @@
+const Product = require("../models/product")
+
+const getAllProductStatic = async(req, res) => {
+    const search = 'ab'
+    const products = await Product.find({}).sort('-name')
+    res.status(200).json({products, nbHits: products.length}) 
+}
+
+const getAllProduct = async(req, res) => {
+    const {featured, company, name, sort, fields} = req.query
+    const queryObject = {}
+    if(featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    if(company) {
+        queryObject.company = company
+    }
+    if(name) {
+        queryObject.name = {$regex: name, $options: 'i'}
+    }
+  //console.log(queryObject)
+    let result = Product.find(queryObject)
+    if(sort) {
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('createdAt')
+    }
+
+    if(fields) {
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)
+    }
+    const products = await result;
+    res.status(200).json({products, nbHits: products.length})
+}
+
+module.exports = {
+    getAllProductStatic,
+    getAllProduct
+}
